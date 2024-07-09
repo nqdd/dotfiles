1064e1fcdfaab00c55e99caa92cd33c4eda413cc
Dat Nguyen Quoc <datnq1210@gmail.com>
2024-07-09 11:18:06 +0700

    chore(bufferline): buffer title style

--- nvim/.config/nvim/lua/nqdat/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/nqdat/plugins/bufferline.lua
@@ -22,48 +22,6 @@ return {
         show_close_icon = false,
         show_tab_indicators = true,
       },
-      highlights = {
-        fill = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "StatusLineNC" },
-        },
-        background = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "StatusLine" },
-        },
-        buffer_visible = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        buffer_selected = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        separator = {
-          fg = { attribute = "bg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "StatusLine" },
-        },
-        separator_selected = {
-          fg = { attribute = "fg", highlight = "Special" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        separator_visible = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "StatusLineNC" },
-        },
-        close_button = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "StatusLine" },
-        },
-        close_button_selected = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        close_button_visible = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-      },
     })
   end,
 }
