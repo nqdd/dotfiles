01d3f090879b8bfd1581de943e708f9c52c1f3a1
Dat Nguyen Quoc <datnq1210@gmail.com>
2024-07-09 11:21:47 +0700

    chore(auto-session): enable

--- nvim/.config/nvim/lua/nqdat/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/nqdat/plugins/auto-session.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local auto_session = require("auto-session")
     auto_session.setup({
-      auto_restore_enabled = false,
+      auto_restore_enabled = true,
       -- ignore dirs
       auto_session_suppress_dirs = {
         "~/",
@@ -14,14 +14,12 @@ return {
         "~/Pictures/",
         "~/Music/",
         "~/Public/",
-        "~/dev/"
-      }
+      },
     })
 
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
     keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
-
   end,
 }
