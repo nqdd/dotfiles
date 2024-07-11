ac1c681d303056b7fc66b578eb0424dbede3207e
Dat Nguyen Quoc <datnq1210@gmail.com>
2024-07-12 01:12:21 +0700

    chore (formatting): change keymap

--- nvim/.config/nvim/lua/nqdat/plugins/formatting.lua
+++ nvim/.config/nvim/lua/nqdat/plugins/formatting.lua
@@ -22,7 +22,7 @@ return {
       },
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+    vim.keymap.set({ "n", "v" }, "<leader>cf", function()
       conform.format({
         lsp_fallback = false,
         async = false,
