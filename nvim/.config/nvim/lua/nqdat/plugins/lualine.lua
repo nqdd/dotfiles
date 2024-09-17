a92015c3e21599d6a8c9337e47479d481d54973e
Dat Nguyen Quoc <datnq1210@gmail.com>
2024-09-17 16:47:15 +0700

    update configs

--- nvim/.config/nvim/lua/nqdat/plugins/lualine.lua
+++ nvim/.config/nvim/lua/nqdat/plugins/lualine.lua
@@ -1,14 +1,20 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      icons_enabled = true,
-      disabled_filetypes = {},
-      globalstatus = true,
-      refresh = {
-        statusline = 100,
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "visual_studio_code",
+        icons_enabled = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {},
+        globalstatus = true,
+        refresh = {
+          statusline = 100,
+        },
       },
-    },
-  },
+      sections = require("visual_studio_code").get_lualine_sections(),
+    })
+  end,
 }
